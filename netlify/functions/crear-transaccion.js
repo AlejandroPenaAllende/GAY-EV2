4611bff9a7264430f8def6a7f889b871cc8969da
AlejandroPenaAllende <alejandro.pena.allende2304@gmail.com>
2025-06-10 09:37:38 -0400

    Update crear-transaccion.js

--- netlify/functions/crear-transaccion.js
+++ netlify/functions/crear-transaccion.js
@@ -11,6 +11,8 @@ exports.handler = async (event, context) => {
     "return_url": "https://webhook.site/0fd266dc-90de-44d7-ad07-ea1edc4a19fb"
   };
 
+  console.log(return_url)
+
   try {
     const response = await fetch(url, {
       method: 'POST',
