ccac39ec6755a10d719afb5559d151f4ff31d48b
AlejandroPenaAllende <alejandro.pena.allende2304@gmail.com>
2025-06-10 09:44:59 -0400

    Add files via upload

---
+++ netlify/functions/retorno.js
@@ -0,0 +1,11 @@
+exports.handler = async (event) => {
+  const params = event.queryStringParameters;
+  const token = params.token_ws;
+
+  return {
+    statusCode: 302,
+    headers: {
+      Location: `https://masagua.netlify.app/result.html?token_ws=${token}`
+    }
+  };
+};
